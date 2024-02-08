4999b934c2560f7b0f8dd9dac408be922e970720
Gleb <naumov.gm@mail.ru>
2024-02-08 21:03:35 +0300

    Frontend
    - Add react project
    - Run express with webpack

---
+++ frontend/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Обслуживание статических файлов из папки build
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Обработка всех остальных запросов отправляет index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+// Запуск Express сервера
+app.listen(port, () => {
+    console.log(`Сервер запущен на порте ${port}`);
+});
